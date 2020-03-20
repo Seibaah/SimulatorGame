286d595ee146d7e3f0160f5b71cecffcc13f2dac
Seibaah <dalexandre_12@outlook.com>
2020-03-19 23:22:13 -0400

    scaling to 100x100 grid; fixed incomplete range coverage bug; re-assigned palette to numerical ranges to smooth terrain visually

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -40,16 +40,20 @@ public:
 
 class world2D {
 public:
-	int m[20][20] = { 0 };
+	int m[100][100] = { 0 };
+	int mSize = size(m);
+	int chunks[10][10];
+	int chunksSize = size(chunks);
 	int spot;
+	int smallest = 0, biggest = 0;
 	player p1;
 
-	SDL_Color palette[21] = { {25, 25, 112, 0}, {0, 0, 128, 0}, {0, 0, 205, 0}, {0, 0, 225, 0}, {0, 0, 255, 0}, {45, 100, 245, 0},
+	SDL_Color palette[20] = { {25, 25, 112, 0}, {0, 0, 128, 0}, {0, 0, 205, 0}, {0, 0, 225, 0}, {0, 0, 255, 0}, {45, 100, 245, 0},
 								{51, 171, 240, 0}, {82, 219, 255, 0}, {110, 255, 255, 0}, {168, 255, 255}, {227, 220, 192, 0},
-								{219, 173, 114, 0}, {202, 143, 66, 0}, {124, 252, 0, 0}, {34, 139, 34, 0}, {0, 100, 0, 0},
+								{219, 173, 114, 0}, {124, 252, 0, 0}, {34, 139, 34, 0}, {0, 100, 0, 0},
 								{193, 210, 214, 0}, {174, 187, 199, 0}, {106, 125, 142, 0}, {105, 105, 105, 0}, {240, 240, 240, 0} };
-	int grid_cell_size = 20;
-	int grid_width = 20, grid_height = 20;
+	int grid_cell_size = 5;
+	int grid_width = 100, grid_height = 100;
 	int window_height = (grid_cell_size * grid_height) + 1;
 	int window_width = (grid_cell_size * grid_width) + 1;
 	SDL_Color grid_line_color = { 255, 255, 255, 255 }; //White
@@ -68,13 +72,13 @@ public:
 	//Algorithm to generate terrain TODO: Refine
 	int loadWorld() {
 		//fill 2d array
-		int chunks[10][10];
+		
 		int coord[100][2], count = 0;
 		int waterCoord[100][2], waterCount = 0;
 		random_device rd;           //Will be used to obtain a seed for the random number engine
 		mt19937 gen(rd());          //Standard mersenne_twister_engine seeded with rd()
-		uniform_real_distribution<> disSeedOnMap(0.2f, 1.0);         //Distribution for seed probability
-		uniform_real_distribution<> disSeedInChunk(0.0, 2.0);        //Distribution for seed within chunk
+		uniform_real_distribution<> disSeedOnMap(0.4, 0.66);         //Distribution for seed probability
+		uniform_real_distribution<> disSeedInChunk(0.0, 11.0);        //Distribution for seed within chunk
 		uniform_real_distribution<> disWaterSeed(0.0, 2.0);        //Distribution for water seed
 		double baseCoef = (double)((int)(disSeedOnMap(gen) * 100)) / 100;
 		double waterCoef = (double)((int)(disWaterSeed(gen) * 100)) / 100;
@@ -82,23 +86,23 @@ public:
 		cout << baseCoef << endl;
 
 		//marks which chunks will hold a seed for the world gen to use later
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < chunksSize; i++) {
+			for (int j = 0; j < chunksSize; j++) {
 				double seedProb = (double)((int)(disSeedOnMap(gen) * 100)) / 100;
 				if (seedProb <= baseCoef) {
 					chunks[i][j] = 1;
-					m[i * 2 + (int)disSeedInChunk(gen)][j * 2 + (int)disSeedInChunk(gen)] = 1;
+					m[i * chunksSize + (int)disSeedInChunk(gen)][j * chunksSize + (int)disSeedInChunk(gen)] = 1;
 					//save seed coord
-					coord[count][0] = i * 2 + (int)disSeedInChunk(gen); //y
-					coord[count][1] = j * 2 + (int)disSeedInChunk(gen); //x
+					coord[count][0] = i * chunksSize + (int)disSeedInChunk(gen); //y
+					coord[count][1] = j * chunksSize + (int)disSeedInChunk(gen); //x
 					count++;
 				}
 				else {
 					chunks[i][j] = -1;
-					m[i * 2 + (int)disSeedInChunk(gen)][j * 2 + (int)disSeedInChunk(gen)] = -1;
+					m[i * chunksSize + (int)disSeedInChunk(gen)][j * chunksSize + (int)disSeedInChunk(gen)] = -1;
 					//save seed coord
-					waterCoord[waterCount][0] = i * 2 + (int)disSeedInChunk(gen); //y
-					waterCoord[waterCount][1] = j * 2 + (int)disSeedInChunk(gen); //x
+					waterCoord[waterCount][0] = i * chunksSize + (int)disSeedInChunk(gen); //y
+					waterCoord[waterCount][1] = j * chunksSize + (int)disSeedInChunk(gen); //x
 					waterCount++;
 				}
 				
@@ -116,9 +120,9 @@ public:
 				}*/
 			}
 		}
-		//printing seed map
-		/*for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 20; j++) {
+		//printing seed coords
+		/*for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
 				if (m[i][j] >= 0) {
 					cout << "| " << m[i][j] << '|';
 				}
@@ -130,17 +134,18 @@ public:
 		//height variation
 		uniform_real_distribution<> disHeight(0, 2.0);
 		//time value
-		uniform_real_distribution<> disRange(3.0, 7.0);
+		uniform_real_distribution<> disRange(2.0, 5.0);
 		for (int n = 0; n < count; n++) {
 			int y = coord[n][0], x = coord[n][1];
 			double time = (int)disRange(gen);
 			for (int t = 0; t < time; t++) {
-				for (int y2 = y - t; y2 <= y; y2++) {
-					for (int x2 = x - t; x2 <= x; x2++) {
-						if ((y2 >= 0 && y2 < 20) && (x2 >= 0 && x2 < 20)) {
+				for (int y2 = y - t; y2 <= 2*y; y2++) {
+					for (int x2 = x - t; x2 <= 2*x; x2++) {
+						if ((y2 >= 0 && y2 < mSize) && (x2 >= 0 && x2 < mSize)) {
 							//int var = (int)disHeight(gen);
 							if (t == time - 1) {
 								int var = (int)disHeight(gen);
+								m[y2][x2] += var;
 							} else	m[y2][x2] += 1;
 						}
 						//else ignore out of bounds coords
@@ -151,17 +156,18 @@ public:
 		//deep variation
 		uniform_real_distribution<> disDeep(-1.99, 0.0);
 		//time2 value
-		uniform_real_distribution<> disRange2(3.0, 7.0);
+		uniform_real_distribution<> disRange2(2.0, 5.0);
 		for (int n = 0; n < waterCount; n++) {
 			int y = waterCoord[n][0], x = waterCoord[n][1];
 			double time2 = (int)disRange2(gen);
 			for (int t = 0; t < time2; t++) {
-				for (int y2 = y - t; y2 <= y; y2++) {
-					for (int x2 = x - t; x2 <= x; x2++) {
-						if ((y2 >= 0 && y2 < 20) && (x2 >= 0 && x2 < 20)) {
+				for (int y2 = y - t; y2 <= 2*y; y2++) {
+					for (int x2 = x - t; x2 <= 2*x; x2++) {
+						if ((y2 >= 0 && y2 < mSize) && (x2 >= 0 && x2 < mSize)) {
 							//int var = (int)disDeep(gen);
 							if (t == time2 - 1) {
 								int var = (int)disDeep(gen);
+								m[y2][x2] -= var;
 							}
 							else	m[y2][x2] -= 1;
 						}
@@ -182,7 +188,7 @@ public:
 		}*/
 
 		//place p1 at the center and save overwritten tile
-		p1.col = 10; p1.row = 10; p1.down;
+		p1.col = mSize/2; p1.row = mSize/2; p1.down;
 		//spot = m[p1.row][p1.col];
 		//m[p1.row][p1.col] = 99;
 
@@ -202,139 +208,111 @@ public:
 
 	//Print world to console. For debug purposes.
 	void displayWorld() {
-		//diplay text world to console
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 20; j++) {
-				if (m[i][j] >= 0) {
-					cout << "| " << m[i][j] << '|';
+		
+		//tracking max and min height on map
+		for (int i = 0; i < mSize; i++) {
+			for (int j = 0; j < mSize; j++) {
+				if (m[i][j] > biggest) {
+					biggest=m[i][j];
 				}
-				else cout << '|' << m[i][j] << '|';
-			} cout << endl;
+				else if (m[i][j] < smallest) {
+					smallest = m[i][j];
+				}
+			};
 		}
-		cout << "-------------------------------------------" << endl;
-
+		
+		//cout << "-------------------------------------------" << endl;
+		
 		//Rendering individual squares. Color changes based on value.
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 20; j++) {
-				SDL_Rect tile = { (j * grid_width), (i * grid_width), grid_cell_size, grid_cell_size };
-				switch (m[i][j]) {
-					case -9:{
-						SDL_SetRenderDrawColor(renderer, palette[1].r, palette[1].g, palette[1].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -8: {
-						SDL_SetRenderDrawColor(renderer, palette[2].r, palette[2].g, palette[2].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -7: {
-						SDL_SetRenderDrawColor(renderer, palette[3].r, palette[3].g, palette[3].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -6: {
-						SDL_SetRenderDrawColor(renderer, palette[4].r, palette[4].g, palette[4].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -5: {
-						SDL_SetRenderDrawColor(renderer, palette[5].r, palette[6].g, palette[6].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -4: {
-						SDL_SetRenderDrawColor(renderer, palette[7].r, palette[7].g, palette[7].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -3: {
-						SDL_SetRenderDrawColor(renderer, palette[8].r, palette[8].g, palette[8].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -2: {
-						SDL_SetRenderDrawColor(renderer, palette[9].r, palette[9].g, palette[9].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case -1: {
-						SDL_SetRenderDrawColor(renderer, palette[10].r, palette[10].g, palette[10].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 0: {
-						SDL_SetRenderDrawColor(renderer, palette[11].r, palette[11].g, palette[11].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 1: {
-						SDL_SetRenderDrawColor(renderer, palette[12].r, palette[12].g, palette[12].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 2: {
-						SDL_SetRenderDrawColor(renderer, palette[13].r, palette[13].g, palette[13].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 3: {
-						SDL_SetRenderDrawColor(renderer, palette[14].r, palette[14].g, palette[14].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 4: {
-						SDL_SetRenderDrawColor(renderer, palette[15].r, palette[15].g, palette[15].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 5: {
-						SDL_SetRenderDrawColor(renderer, palette[16].r, palette[16].g, palette[16].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 6: {
-						SDL_SetRenderDrawColor(renderer, palette[17].r, palette[17].g, palette[17].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 7: {
-						SDL_SetRenderDrawColor(renderer, palette[18].r, palette[18].g, palette[18].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 8: {
-						SDL_SetRenderDrawColor(renderer, palette[19].r, palette[19].g, palette[19].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					case 9: {
-						SDL_SetRenderDrawColor(renderer, palette[20].r, palette[20].g, palette[20].b, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}
-					/*case 99: {
-						SDL_SetRenderDrawColor(renderer, 228, 0, 224, 0);
-						SDL_RenderFillRect(renderer, &tile);
-						break;
-					}*/
-					default: {
-						if (m[i][j]<=-10){
-							SDL_SetRenderDrawColor(renderer, palette[0].r, palette[0].g, palette[0].b, 0);
-							SDL_RenderFillRect(renderer, &tile);
-						} else {
-							SDL_SetRenderDrawColor(renderer, palette[20].r, palette[20].g, palette[20].b, 0);
-							SDL_RenderFillRect(renderer, &tile);
-						}
-						break;
-					}
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				SDL_Rect tile = { (j * grid_cell_size), (i * grid_cell_size), grid_cell_size, grid_cell_size };
+				if (m[i][j] <= -41) {
+					SDL_SetRenderDrawColor(renderer, palette[0].r, palette[0].g, palette[0].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				} 
+				else if (m[i][j] <= -36 && m[i][j] >= -40) {
+					SDL_SetRenderDrawColor(renderer, palette[1].r, palette[1].g, palette[1].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				} 
+				else if (m[i][j] <= -31 && m[i][j] >= -35) {
+					SDL_SetRenderDrawColor(renderer, palette[2].r, palette[2].g, palette[2].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				} 
+				else if (m[i][j] <= -26 && m[i][j] >= -30) {
+					SDL_SetRenderDrawColor(renderer, palette[3].r, palette[3].g, palette[3].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				} 
+				else if (m[i][j] <= -21 && m[i][j] >= -25) {
+					SDL_SetRenderDrawColor(renderer, palette[4].r, palette[4].g, palette[4].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				} 
+				else if (m[i][j] <= -16 && m[i][j] >= -20) {
+					SDL_SetRenderDrawColor(renderer, palette[5].r, palette[5].g, palette[5].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] <= -11 && m[i][j] >= -15) {
+					SDL_SetRenderDrawColor(renderer, palette[6].r, palette[6].g, palette[6].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] <= -6 && m[i][j] >= -10) {
+					SDL_SetRenderDrawColor(renderer, palette[7].r, palette[7].g, palette[7].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] <= -3 && m[i][j] >= -5) {
+					SDL_SetRenderDrawColor(renderer, palette[8].r, palette[8].g, palette[8].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] <= -1 && m[i][j] >= -2) {
+					SDL_SetRenderDrawColor(renderer, palette[9].r, palette[9].g, palette[9].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] == 0) {
+					SDL_SetRenderDrawColor(renderer, palette[10].r, palette[10].g, palette[10].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 1 && m[i][j] <= 2) {
+					SDL_SetRenderDrawColor(renderer, palette[11].r, palette[11].g, palette[11].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 2 && m[i][j] <= 5) {
+					SDL_SetRenderDrawColor(renderer, palette[12].r, palette[12].g, palette[12].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 6 && m[i][j] <= 10) {
+					SDL_SetRenderDrawColor(renderer, palette[13].r, palette[13].g, palette[13].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 11 && m[i][j] <= 15) {
+					SDL_SetRenderDrawColor(renderer, palette[14].r, palette[14].g, palette[14].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 16 && m[i][j] <= 20) {
+					SDL_SetRenderDrawColor(renderer, palette[15].r, palette[15].g, palette[15].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 21 && m[i][j] <= 25) {
+					SDL_SetRenderDrawColor(renderer, palette[16].r, palette[16].g, palette[16].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 26 && m[i][j] <= 32) {
+					SDL_SetRenderDrawColor(renderer, palette[17].r, palette[17].g, palette[17].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else if (m[i][j] >= 33 && m[i][j] <= 40) {
+					SDL_SetRenderDrawColor(renderer, palette[18].r, palette[18].g, palette[18].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
+				}
+				else {
+					SDL_SetRenderDrawColor(renderer, palette[19].r, palette[19].g, palette[19].b, 0);
+					SDL_RenderFillRect(renderer, &tile);
 				}
 			}
 		}
 
 		renderPlayer();
 
-		SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
+		/*SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
 			grid_line_color.b, grid_line_color.a);
 
 		for (int x = 0; x < 1 + grid_width * grid_cell_size;
@@ -344,7 +322,7 @@ public:
 		for (int y = 0; y < 1 + grid_height * grid_cell_size;
 			y += grid_cell_size) {
 			SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-		}
+		}*/
 		SDL_RenderClear; //clear screen
 		SDL_RenderPresent(renderer); //Render backbuffer
 		
@@ -353,7 +331,7 @@ public:
 	//Renders player
 	void renderPlayer() {
 		SDL_Rect playerRender = { (this->p1.col*grid_cell_size), (this->p1.row*grid_cell_size), grid_cell_size / 2, grid_cell_size / 2 };
-		SDL_SetRenderDrawColor(renderer, 228, 0, 224, 0);
+		SDL_SetRenderDrawColor(renderer, 228, 0, 224, 0); //purple
 		SDL_RenderFillRect(renderer, &playerRender);
 	}
 
@@ -452,5 +430,7 @@ int main() {
 	test.loadWorld();
 	test.displayWorld();
 	test.simLoop();
+	cout << test.smallest << endl;
+	cout << test.biggest << endl;
 	return 0;
 }
